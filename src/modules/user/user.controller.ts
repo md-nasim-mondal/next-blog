08cec9f247082d2d434f609c0022d90b97e92fe4
md-nasim-mondal <nasim.programmer24@gmail.com>
2025-10-20 01:40:18 +0600

    make some user apis like create user, get all user, get single user by id

---
+++ src/modules/user/user.controller.ts
@@ -0,0 +1,35 @@
+import type { Request, Response } from "express";
+import { UserService } from "./user.service";
+
+const createUser = async (req: Request, res: Response) => {
+  try {
+    const result = await UserService.createUser(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+const getAllFromDB = async (req: Request, res: Response) => {
+  try {
+    const result = await UserService.getAllFromDB();
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+const getUserById = async (req: Request, res: Response) => {
+  try {
+    const result = await UserService.getUserById(Number(req.params.id));
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+export const UserController = {
+  createUser,
+  getAllFromDB,
+  getUserById,
+};
---
+++ src/modules/user/user.route.ts
@@ -0,0 +1,12 @@
+import  express  from "express";
+import { UserController } from "./user.controller";
+
+const router = express.Router();
+
+router.get("/", UserController.getAllFromDB);
+router.get("/:id", UserController.getUserById);
+router.get("/", UserController.getAllFromDB);
+
+router.post("/", UserController.createUser);
+
+export const userRouter = router;
