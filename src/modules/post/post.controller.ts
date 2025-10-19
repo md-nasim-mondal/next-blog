b70e5782087caa2cbc98a2eb26eb2c39dc20422c
md-nasim-mondal <nasim.programmer24@gmail.com>
2025-10-20 02:21:03 +0600

    implement searching to get all post api

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -14,7 +14,9 @@ const getAllPosts = async (req: Request, res: Response) => {
   try {
     const page = Number(req.query.page) || 1;
     const limit = Number(req.query.limit) || 10;
-    const result = await PostService.getAllPosts({ page, limit });
+    const search = (req.query.search as string) || "";
+    
+    const result = await PostService.getAllPosts({ page, limit, search });
     res.json(result);
   } catch (err) {
     res.status(500).json({ error: "Failed to fetch posts", details: err });
