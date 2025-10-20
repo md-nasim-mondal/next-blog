2a2ab19e0aba76c726455187cd592e1c71a5af23
md-nasim-mondal <nasim.programmer24@gmail.com>
2025-10-21 01:43:41 +0600

    implement searching and fieltering

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -19,34 +19,36 @@ const createPost = async (payload: Prisma.PostCreateInput): Promise<Post> => {
 };
 
 const getAllPosts = async ({
-  page,
-  limit,
+  page = 1,
+  limit = 10,
   search,
+  isFeatured,
+  tags,
 }: {
-  page: number;
-  limit: number;
-  search: string;
+  page?: number;
+  limit?: number;
+  search?: string;
+  isFeatured?: boolean;
+  tags?: string[];
 }) => {
   const skip = (page - 1) * limit;
+  const where: any = {
+    AND: [
+      search && {
+        OR: [
+          { title: { contains: search, mode: "insensitive" } },
+          { content: { contains: search, mode: "insensitive" } },
+        ],
+      },
+      typeof isFeatured === "boolean" && { isFeatured },
+      tags && tags.length > 0 && { tags: { hasEvery: tags } },
+    ].filter(Boolean),
+  };
+
   const result = await prisma.post.findMany({
     skip,
     take: limit,
-    where: {
-      OR: [
-        {
-          title: {
-            contains: search,
-            mode: "insensitive",
-          },
-        },
-        {
-          content: {
-            contains: search,
-            mode: "insensitive",
-          },
-        },
-      ],
-    },
+    where,
   });
   return result;
 };
